7c023942903ee0a6d298fac468ce0b7874114498
JefferyWang <admin@wangjunfeng.com>
2025-11-25 10:17:03 +0800

    add rotate process logic

--- src/image/rotation.rs
+++ src/image/rotation.rs
@@ -1,6 +1,7 @@
 use std::{fmt::Display, str::FromStr};
 
 use image::DynamicImage;
+use image::GenericImageView;
 
 use crate::IiifError;
 
@@ -40,16 +41,58 @@ impl Rotation {
                 if *angle < 0.0 || *angle > 360.0 {
                     return Err(IiifError::InvalidRotationFormat(self.to_string()));
                 }
-                Ok(image.rotate90())
+                if is_multiple_of_90(*angle) {
+                    return Ok(standard_rotate(image, *angle));
+                }
+                Ok(rotate(image, *angle))
             }
             Rotation::MirrorDegrees(angle) => {
                 if *angle < 0.0 || *angle > 360.0 {
                     return Err(IiifError::InvalidRotationFormat(self.to_string()));
                 }
-                Ok(image.fliph().rotate90())
+                let image = image.fliph();
+                if is_multiple_of_90(*angle) {
+                    return Ok(standard_rotate(image, *angle));
+                }
+                Ok(rotate(image, *angle))
             }
         }
     }
+}
+
+// 判断是否是 0/90/180/270 的倍数
+fn is_multiple_of_90(angle: f32) -> bool {
+    angle % 90.0 == 0.0
+}
+
+// 针对 0/90/180/270 的倍数，进行优化
+fn standard_rotate(image: DynamicImage, angle: f32) -> DynamicImage {
+    match angle {
+        0.0 => image,
+        90.0 => image.rotate90(),
+        180.0 => image.rotate180(),
+        270.0 => image.rotate270(),
+        _ => image,
+    }
+}
+
+fn rotate(image: DynamicImage, angle: f32) -> DynamicImage {
+    // 旋转角度转换为弧度
+    let angle = angle * std::f32::consts::PI / 180.0;
+    // 计算旋转后的图片大小
+    let new_width = (image.width() as f32 * angle.cos() + image.height() as f32 * angle.sin()).round() as u32;
+    let new_height = (image.width() as f32 * angle.sin() + image.height() as f32 * angle.cos()).round() as u32;
+    let mut rotated_image = image::ImageBuffer::new(new_width, new_height);
+    for x in 0..image.width() {
+        for y in 0..image.height() {
+            let new_x = x + ((new_width as f32 - image.width() as f32) / 2.0).round() as u32;
+            let new_y = y + ((new_height as f32 - image.height() as f32) / 2.0).round() as u32;
+            let pixel = image.get_pixel(x, y);
+            rotated_image.put_pixel(new_x, new_y, pixel);
+        }
+    }
+    let rotated_image = imageproc::geometric_transformations::rotate_about_center(&rotated_image, angle, imageproc::geometric_transformations::Interpolation::Bicubic, image::Rgba([0, 0, 0, 0]));
+    image::DynamicImage::ImageRgba8(rotated_image)
 }
 
 impl FromStr for Rotation {
